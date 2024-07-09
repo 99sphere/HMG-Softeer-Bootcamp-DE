76b673a7cb7bbafaafa7e3be05e693110f1231bd
Gu Lee <99sphere@gmail.com>
2024-07-09 18:20:54 +0900

    feat: mission 5 (with youtube comment crawling)

--- Mission/W2/mission4.py
+++ Mission/W2/mission4.py
@@ -1,34 +1,37 @@
 import time
-from multiprocessing import Pool, Queue, Process, current_process, Manager
+from multiprocessing import Pool, Queue, Process, current_process, Manager, JoinableQueue
 
+NUM_TASK = 10
 NUM_PROCESSES = 4
 
 def target_func(tasks_to_accomplish, tasks_that_are_done):
     try:
-        val = tasks_to_accomplish.get_nowait()
-        print(f"Task no {val}")
-        time.sleep(0.5)
-        print(f"Task no {val} is done by {current_process().name}")
-        tasks_that_are_done.put(f"Task no {val} is done by Process-{current_process().name[-1]}")
+        while True:
+            val = tasks_to_accomplish.get_nowait()
+            print(f"Task no {val}")
+            time.sleep(0.5)
+            tasks_that_are_done.put(f"Task no {val} is done by {current_process().name}")
     except Exception as e: 
-        print(e)            
+        pass          
     return
 
 if __name__=="__main__":
-    with Manager() as manager: # for shared queue
-        tasks_to_accomplish = manager.Queue()
-        tasks_that_are_done = manager.Queue()
-        for i in range(10):
-            tasks_to_accomplish.put(i)
-            
-        while not tasks_to_accomplish.empty():
-        # producer process 선언
-            procs=[]
-            for i in range(NUM_PROCESSES):
-                procs.append(Process(name=f'Process-{i+1}', target=target_func, args=(tasks_to_accomplish, tasks_that_are_done,)))
-
-            for proc in procs:
-                proc.start()
+    tasks_to_accomplish = Queue()
+    tasks_that_are_done = Queue()
+    procs = []
+    
+    for i in range(NUM_TASK):
+        tasks_to_accomplish.put(i)
+    
+    for i in range(NUM_PROCESSES):
+        proc = Process(name=f'Process-{i+1}', target=target_func, args=(tasks_to_accomplish, tasks_that_are_done,))
+        procs.append(proc) 
+        procs[i].start()
                 
-            for proc in procs:
-                proc.join()
+    # Wait for all processes to finish.
+    for proc in procs:
+        proc.join()
+        
+    for i in range(NUM_TASK):
+        msg = tasks_that_are_done.get()
+        print(msg)
