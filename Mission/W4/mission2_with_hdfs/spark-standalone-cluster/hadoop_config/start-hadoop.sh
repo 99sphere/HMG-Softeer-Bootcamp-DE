4b04f21e54335926a28b4bec4be31a9fee73e242
Gu Lee <99sphere@gmail.com>
2024-07-29 18:32:15 +0900

    0729

--- Mission/W4/mission2_with_hdfs/spark-standalone-cluster/hadoop_config/start-hadoop.sh
+++ Mission/W4/mission2_with_hdfs/spark-standalone-cluster/hadoop_config/start-hadoop.sh
@@ -3,15 +3,13 @@
 echo "$HOSTNAME Start"
 
 if [[ "$HOSTNAME" == "spark-master" ]]; then
-  if [ ! -d "/root/data/namenode/" ]; then
+  if [ ! -d "/home/hduser/data/namenode/" ]; then
     $HADOOP_HOME/bin/hdfs namenode -format && echo "OK : HDFS namenode format operation finished successfully !"
   fi
   $HADOOP_HOME/bin/yarn --daemon start resourcemanager
   $HADOOP_HOME/bin/hdfs --daemon start namenode
   $HADOOP_HOME/bin/hdfs --daemon start secondarynamenode
-  tail -f /dev/null
 else
   $HADOOP_HOME/bin/yarn --daemon start nodemanager
   $HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
-  tail -f /dev/null
 fi
