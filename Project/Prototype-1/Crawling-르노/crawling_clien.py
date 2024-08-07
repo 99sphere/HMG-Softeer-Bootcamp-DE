a4669e56180066d7ce83f93fec4dd9869ff24875
Gu Lee <99sphere@gmail.com>
2024-08-07 19:04:13 +0900

    0807

--- Project/Prototype-1/Crawling-르노/crawling_clien.py
+++ Project/Prototype-1/Crawling-르노/crawling_clien.py
@@ -68,6 +68,12 @@ def per_post_crawling(driver):
     like_cnt_element = driver.find_element(By.CSS_SELECTOR, 'a.symph_count > strong')
     like_cnt = int(like_cnt_element.text)
 
+    # updated_at
+    updated_at = None
+    updated_at_elem = driver.find_elements(By.CLASS_NAME, "lastdate")
+    if len(updated_at_elem)!=0:
+        updated_at = updated_at_elem[0].text.split(" : ")[1].strip()
+        
     # get view count
     view_cnt_elem = driver.find_element(By.CSS_SELECTOR, 'span.view_count > strong')
     view_cnt = int(view_cnt_elem.text.replace(',', ''))
@@ -85,15 +91,19 @@ def per_post_crawling(driver):
     cmt_authors = []
     cmt_contents = []
     cmt_post_ids = []    
-    
+    cmt_created_ats = []
+    cmt_updated_ats = []
+
     cmt_ad_banner_elem = driver.find_element(By.CLASS_NAME, "comment.ad_banner")
     cmt_authors_elem = cmt_ad_banner_elem.find_elements(By.CLASS_NAME, "nickname")
     cmt_gif_authors_elem = cmt_ad_banner_elem.find_elements(By.CSS_SELECTOR, 'span.nickname img') # for gif nickname
     cmt_contents_elem = cmt_ad_banner_elem.find_elements(By.CLASS_NAME, "comment_view")
     cmt_gif_authors_cnt = 0
-    
+    cmt_created_at_elem = cmt_ad_banner_elem.find_elements(By.CLASS_NAME, "timestamp")
+
     assert len(cmt_authors_elem)==len(cmt_contents_elem)    
     cmt_cnt = len(cmt_authors_elem)
+    
     for idx in range(cmt_cnt):
         cmt_author = cmt_authors_elem[idx].text
         if cmt_author=='': # for gif nickname
@@ -101,20 +111,40 @@ def per_post_crawling(driver):
             cmt_gif_authors_cnt += 1
         cmt_authors.append(cmt_author)        
         cmt_contents.append(cmt_contents_elem[idx].text)
+        cmt_datetime_info = cmt_created_at_elem[idx].get_attribute('textContent')
+        cmt_updated_at = None
+        if "/" in cmt_datetime_info:
+            cmt_created_at, cmt_updated_at = cmt_datetime_info.split("/")
+            cmt_updated_at = cmt_updated_at.split(": ")[1]
+        else:
+            cmt_created_at = cmt_datetime_info
+        cmt_created_at = cmt_created_at.strip()
+        
+        if cmt_updated_at != None:
+            cmt_updated_at = cmt_updated_at.strip()
+            
+        cmt_created_ats.append(cmt_created_at)
+        cmt_updated_ats.append(cmt_updated_at)
         cmt_post_ids.append(post_id)
     
-    return content, like_cnt, view_cnt, cmt_authors, cmt_contents, cmt_post_ids, cmt_cnt, post_id
+    return content, like_cnt, view_cnt, cmt_authors, cmt_contents, cmt_post_ids, cmt_created_ats, cmt_updated_ats, cmt_cnt, post_id, updated_at
 
 
 if __name__=="__main__":
-    datetime_start='2024-06-20 00:00:00'
-    datetime_end='2024-07-20 23:59:59'
-    
     started=False
     end=False
-    output_dir='/Users/admin/Desktop/HMG/ws/Project/Crawling/output/'
-    post_file_name='clien-르노-posts.csv'
-    comments_file_name='clien-르노-comments.csv'
+    output_dir='/Users/admin/Desktop/HMG/ws/Project/Prototype-1/Crawling-르노/output/'
+
+    # datetime_start='2024-06-20 00:00:00'
+    # datetime_end='2024-07-20 23:59:59'
+    # post_file_name='clien-르노-posts.csv'
+    # comments_file_name='clien-르노-comments.csv'
+    
+    datetime_start='2024-06-29 00:00:00'
+    datetime_end='2024-07-29 23:59:59'
+    post_file_name='clien-홍명보-posts.csv'
+    comments_file_name='clien-홍명보-comments.csv'
+    
     post_fp = output_dir + post_file_name
     comment_fp = output_dir + comments_file_name
     
@@ -122,9 +152,14 @@ if __name__=="__main__":
     page_num=0
     while not end:
         post_idx = 0
-        search_board_url = f'https://www.clien.net/service/search?q=%EB%A5%B4%EB%85%B8&sort=recency&p={page_num}&boardCd=&isBoard=false'
+        # 르노
+        # search_board_url = f'https://www.clien.net/service/search?q=%EB%A5%B4%EB%85%B8&sort=recency&p={page_num}&boardCd=&isBoard=false'
+        
+        # 홍명보
+        search_board_url = f'https://www.clien.net/service/search?q=%ED%99%8D%EB%AA%85%EB%B3%B4&sort=recency&p={page_num}&boardCd=&isBoard=false'
         driver = open_url(driver, search_board_url)
         titles, urls, created_ats, authors, posts_cnt = search_board_crawling(driver)
+        updated_ats = []
         contents = []
         likes = []
         views = []
@@ -146,18 +181,18 @@ if __name__=="__main__":
                 start_idx = 0
             post_url = urls[post_idx]
             driver = open_url(driver, post_url)
-            content, like_cnt, view_cnt, cmt_authors, cmt_contents, cmt_post_ids, cmt_cnt, post_id = per_post_crawling(driver)
+            content, like_cnt, view_cnt, cmt_authors, cmt_contents, cmt_post_ids, cmt_created_ats, cmt_updated_ats, cmt_cnt, post_id, updated_at = per_post_crawling(driver)
 
             # Save to comments.csv
-            ids = make_dummy_data(cmt_cnt)
-            comment_data = {"id": ids,"post_id": cmt_post_ids, "content": cmt_contents, "author": cmt_authors}
+            comment_data = {"post_id": cmt_post_ids, "cmt_content": cmt_contents, "cmt_author": cmt_authors, "cmt_created_at": cmt_created_ats, "cmt_updated_at": cmt_updated_ats}
             save_dict_to_csv(comment_data, comment_fp)
 
             post_ids.append(post_id)
             contents.append(content)
             likes.append(like_cnt)
             views.append(view_cnt)
-            # print(f"title: {titles[post_idx]}\nnumber of comments: {cmt_cnt}" )
+            updated_ats.append(updated_at)
+            print(f"title: {titles[post_idx]}\nnumber of comments: {cmt_cnt}" )
 
         # Save to posts.csv 
         if end_idx != -1:
@@ -173,9 +208,8 @@ if __name__=="__main__":
             created_ats = created_ats[start_idx:]
             authors = authors[start_idx:]
                     
-        updated_ats = make_dummy_data(len(created_ats))
-        post_data = {"id": post_ids, "created_at": created_ats, "updated_at": updated_ats,"title": titles, \
-            'content': contents, 'likes': likes, 'url': urls, 'author': authors, 'views': views}
+        post_data = {"id": post_ids, "title": titles, 'content': contents, 'likes': likes, 'url': urls, \
+            'author': authors, 'views': views, "created_at": created_ats, "updated_at": updated_ats}
         save_dict_to_csv(post_data, post_fp)
         page_num += 1
     driver.quit()
